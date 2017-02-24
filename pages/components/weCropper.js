f88f810534c68193795453e25a9df28835da92e4
shitao <gowithd@163.com>
2017-02-24 19:29:31 +0800

    2/24

--- pages/components/weCropper.js
+++ pages/components/weCropper.js
@@ -7,40 +7,42 @@ Page({
     maxScale: 2.5,
     src: '../../test.jpg'
   },
-  getDevice () {
+  getDevice() {
     let self = this
     !self.device && (self.device = wx.getSystemInfoSync())
     return self.device
   },
-  initCanvas (src) {
+  initCanvas(src) {
     let self = this
     wx.getImageInfo({
       src,
-      success (res) {
+      success(res) {
         let { id, width, height } = self.data
         let device = self.getDevice()
         let innerAspectRadio = res.width / res.height
 
         self.croperTarget = src
-        self.rectX = 0
-        self.baseWidth= width * device.windowWidth/ 750
+        self.baseWidth = width * device.windowWidth / 750
         self.baseHeight = width * device.windowWidth / (innerAspectRadio * 750)
-        self.rectY = (height * device.windowWidth/ 750 - self.baseHeight) / 2
+        self.rectX = -self.baseWidth / 2
+        self.rectY = (height * device.windowWidth / 750 - self.baseHeight) / 2 - self.baseHeight / 2
         self.scaleWidth = self.baseWidth
         self.scaleHeight = self.baseHeight
         self.oldScale = 1
+        self.rotate = 0
 
         //  画布绘制图片
         self.ctx = wx.createCanvasContext(id)
+        self.ctx.translate(self.baseWidth / 2, self.baseHeight / 2)
         self.ctx.drawImage(src, self.rectX, self.rectY, self.baseWidth, self.baseHeight)
         self.ctx.draw()
       }
     })
   },
   //  图片手势初始监测
-  uploadScaleStart (e) {
+  uploadScaleStart(e) {
     let self = this
-    let xMove, yMove, oldDistance
+    let xMove, yMove, oldDistance, oldSlope
     let [touch0, touch1] = e.touches
 
     //计算第一个触摸点的位置，并参照改点进行缩放
@@ -61,16 +63,20 @@ Page({
       xMove = touch1.x - touch0.x
       yMove = touch1.y - touch0.y
       oldDistance = Math.sqrt(xMove * xMove + yMove * yMove)
+      oldSlope = yMove / xMove
 
       self.oldDistance = oldDistance
+      self.oldSlope = oldSlope
+    }else{
+      self.oldSlope = null
     }
   },
   //  图片手势动态缩放
-  uploadScaleMove (e) {
+  uploadScaleMove(e) {
     let self = this
     let { minScale, maxScale } = self.data
-    let [ touch0, touch1 ] = e.touches
-    let xMove, yMove, newDistance
+    let [touch0, touch1] = e.touches
+    let xMove, yMove, newDistance, newSlope
 
     if (e.timeStamp - self.timeOneFinger < 100) {
       return
@@ -83,12 +89,27 @@ Page({
       if (e.timeStamp - self.timeMoveTwo < 100) {
         return
       }
+
       xMove = touch0.x - self.touchX
       yMove = touch0.y - self.touchY
+
+      var tempX = xMove, tempY = yMove
+      if (self.rotate == 90) {
+        xMove = tempY
+        yMove = -tempX
+      } else if (self.rotate == 180) {
+        xMove = -tempX
+        yMove = -tempY
+      } else if (self.rotate == 270) {
+        xMove = -tempY
+        yMove = tempX
+      }
 
       self.imgLeft = self.rectX + xMove
       self.imgTop = self.rectY + yMove
 
+      self.ctx.translate(self.baseWidth / 2, self.baseHeight / 2)
+      self.ctx.rotate(self.rotate * Math.PI / 180)
       self.ctx.drawImage(self.croperTarget, self.imgLeft, self.imgTop, self.scaleWidth, self.scaleHeight)
       self.ctx.draw()
     }
@@ -99,9 +120,10 @@ Page({
       xMove = touch1.x - touch0.x
       yMove = touch1.y - touch0.y
       newDistance = Math.sqrt(xMove * xMove + yMove * yMove)
+      self.newSlope = yMove / xMove
 
       //  使用0.005的缩放倍数具有良好的缩放体验
-      self.newScale =  self.oldScale + 0.005 * (newDistance - self.oldDistance)
+      self.newScale = self.oldScale + 0.005 * (newDistance - self.oldDistance)
 
       //  设定缩放范围
       self.newScale <= minScale && (self.newScale = minScale)
@@ -109,27 +131,48 @@ Page({
 
       self.scaleWidth = self.newScale * self.baseWidth
       self.scaleHeight = self.newScale * self.baseHeight
-      self.imgLeft =  self.touchX -self.scaleWidth / 2
+      self.imgLeft = self.touchX - self.scaleWidth / 2
       self.imgTop = self.touchY - self.scaleHeight / 2
 
+      self.ctx.translate(self.baseWidth / 2, self.baseHeight / 2)
+      self.ctx.rotate(self.rotate * Math.PI / 180)
       self.ctx.drawImage(self.croperTarget, self.imgLeft, self.imgTop, self.scaleWidth, self.scaleHeight)
       self.ctx.draw()
     }
   },
-  uploadScaleEnd (e) {
+  uploadScaleEnd(e) {
     let self = this
 
     self.oldScale = self.newScale || self.oldScale
     self.rectX = self.imgLeft || self.rectX
     self.rectY = self.imgTop || self.rectY
+
+    if(self.oldSlope){
+      console.log('oldSlope:' + self.oldSlope)
+      var includedAngle = Math.atan(
+        Math.abs(
+          (self.newSlope - self.oldSlope) / (1 - self.newSlope * self.oldSlope)
+        )
+      ) //夹角公式
+      if (includedAngle > 40 * Math.PI / 180) { //大于一定角度才认为发生旋转
+        var direction = self.newSlope > self.oldSlope ? 1 : -1
+        console.log('oldSlope:' + self.oldSlope)
+        self.rotate = ((self.rotate + direction * 90) % 360 + 360) % 360
+
+        self.ctx.translate(self.baseWidth / 2, self.baseHeight / 2)
+        self.ctx.rotate(self.rotate * Math.PI / 180)
+        self.ctx.drawImage(self.croperTarget, self.imgLeft, self.imgTop, self.scaleWidth, self.scaleHeight)
+        self.ctx.draw()
+      }
+    }
   },
-  getCropperImage () {
+  getCropperImage() {
     let self = this
     let { id } = self.data
 
     wx.canvasToTempFilePath({
       canvasId: id,
-      success (res) {
+      success(res) {
         wx.previewImage({
           current: '', // 当前显示图片的http链接
           urls: [res.tempFilePath], // 需要预览的图片http链接列表
@@ -137,7 +180,7 @@ Page({
       }
     })
   },
-  onLoad (option) {
+  onLoad(option) {
     let self = this
     let { src } = self.data
     self.initCanvas(src)
